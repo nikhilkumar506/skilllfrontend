f80c4a41361735cdbf99a90be1b039c15a8c80e4
nikhilKumar506 <nikhilpal04022@gmail.com>
2026-03-09 08:48:46 +0530

    fix session guard

--- frontend/js/core/guards.js
+++ frontend/js/core/guards.js
@@ -1,24 +1,12 @@
 console.log("guards.js loaded");
 
-document.addEventListener("DOMContentLoaded", () => {
+setTimeout(() => {
 
   const token = localStorage.getItem("token");
 
   if (!token) {
     alert("Session expired. Please login again.");
     window.location.href = "../auth/login.html";
-    return;
   }
 
-  /* ===== LOGOUT BUTTON ===== */
-
-  const logoutBtn = document.getElementById("logoutBtn");
-
-  if (logoutBtn) {
-    logoutBtn.addEventListener("click", () => {
-      localStorage.removeItem("token");
-      window.location.href = "../auth/login.html";
-    });
-  }
-
-});
+}, 500);
