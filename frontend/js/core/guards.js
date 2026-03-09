976b5ee684ac85e8b509d59bb3349bb5979b3dc3
nikhilKumar506 <nikhilpal04022@gmail.com>
2026-03-09 08:40:12 +0530

    fix session guard logic

--- frontend/js/core/guards.js
+++ frontend/js/core/guards.js
@@ -1,5 +1,24 @@
-// guards.js
-if (!localStorage.getItem("token")) {
-  alert("Please login first");
-  window.location.href = "../auth/login.html";
-}
+console.log("guards.js loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    alert("Session expired. Please login again.");
+    window.location.href = "../auth/login.html";
+    return;
+  }
+
+  /* ===== LOGOUT BUTTON ===== */
+
+  const logoutBtn = document.getElementById("logoutBtn");
+
+  if (logoutBtn) {
+    logoutBtn.addEventListener("click", () => {
+      localStorage.removeItem("token");
+      window.location.href = "../auth/login.html";
+    });
+  }
+
+});
