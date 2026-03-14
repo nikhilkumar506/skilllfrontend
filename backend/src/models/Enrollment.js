20cae4cc639567a320d21b0d2b291707d0e873f8
nikhilKumar506 <nikhilpal04022@gmail.com>
2026-03-14 18:08:46 +0530

    added process track

--- backend/src/models/Enrollment.js
+++ backend/src/models/Enrollment.js
@@ -1,24 +1,40 @@
 const mongoose = require("mongoose");
 
 const enrollmentSchema = new mongoose.Schema({
-  user: {
+
+  user:{
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
+    ref:"User",
+    required:true
   },
-  courseId: {
-    type: String,
-    required: true
+
+  courseId:{
+    type:String,
+    required:true
   },
-  courseTitle: String,
-  enrolledAt: {
-    type: Date,
-    default: Date.now
+
+  courseTitle:String,
+
+  enrolledAt:{
+    type:Date,
+    default:Date.now
   },
-  completed: {
-    type: Boolean,
-    default: false
+
+  progress:{
+    type:Number,
+    default:0
+  },
+
+  lastLesson:{
+    type:String,
+    default:"lesson1"
+  },
+
+  completed:{
+    type:Boolean,
+    default:false
   }
+
 });
 
 module.exports = mongoose.model("Enrollment", enrollmentSchema);
